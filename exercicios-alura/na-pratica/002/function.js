f58f41f999d98fe3e61f632cca9a9f19e008c5e5
Makalister Mutz Zager <makalistermutzzager@gmail.com>
2026-05-11 18:51:06 -0300

    .

---
+++ exercicios-alura/na-pratica/002/function.js
@@ -0,0 +1,7 @@
+function calcularAreaRetangulo(largura, altura) {
+    return largura * altura;
+}
+
+const calculo = calcularAreaRetangulo(5, 3);
+
+console.log(`A área do retângulo é ${calculo}`);
---
+++ exercicios-alura/na-pratica/002/function2.js
@@ -0,0 +1,6 @@
+function converterCelsiusParaFahrenheit(celsius) {
+    return celsius * 9/5 + 32;
+}
+
+const conversao = converterCelsiusParaFahrenheit(25);
+console.log(conversao);
