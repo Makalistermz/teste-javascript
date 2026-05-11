f6b2d273752c84a43898d4bccc39ee0cac0a1a08
Makalister Mutz Zager <makalistermutzzager@gmail.com>
2026-05-11 17:57:31 -0300

    Create lacofor.js

---
+++ exercicios-alura/na-pratica/001/lacofor.js
@@ -0,0 +1,21 @@
+for(let contador = 1; contador <= 10; contador++) {
+    console.log('Numero atual: ', contador);
+}
+
+for (let numero = 0; numero <= 15; numero++) {
+    if (numero % 2 == 0) {
+        console.log('Numero par encontrado: ', numero);
+    }
+}
+
+for (let numero = 0; numero <= 15; numero++) {
+    if (numero % 2 > 0) {
+        console.log('Numero par encontrado: ', numero);
+    }
+}
+
+const palavra =  'calor';
+
+for (let contador = 0; contador < palavra.length; contador++) {
+    console.log(palavra[contador]);
+}
