5c57b39425fef4b54e780b69528e7bb059895b33
Makalister Mutz Zager <makalistermutzzager@gmail.com>
2026-05-13 19:54:16 -0300

    Update function3.js

--- exercicios-alura/na-pratica/002/function3.js
+++ exercicios-alura/na-pratica/002/function3.js
@@ -4,3 +4,21 @@ function calcularDesconto(preco, percentualDesconto) {
 
 const resultado = calcularDesconto(200, .30);
 console.log(resultado);
+
+function processarCompra(precoProduto, quantidade, temDesconto) {
+
+    const PrecoTotal = processarCompra
+
+    if (precoProduto && quantidade > 0) {
+        return precoProduto * quantidade;
+
+        if (temDesconto === true) {
+
+        } else {
+            return PrecoTotal
+        }
+
+    } else {
+        return "Valores inválidos";
+    }
+}
