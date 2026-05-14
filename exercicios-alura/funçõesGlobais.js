96d724aa9ad7852695be33aea669204d0c225c94
Makalister Mutz Zager <makalistermutzzager@gmail.com>
2026-05-14 16:58:23 -0300

    .

---
+++ exercicios-alura/funçõesGlobais.js
@@ -0,0 +1,15 @@
+const saudacao = () => {
+    console.log('E aí, beleza?');
+}
+
+//setTimeout(saudacao, 2000) // ms (milisegundos)
+
+let contador = 0;
+
+const id = setInterval(() => {
+    contador++
+    console.log('Tempo decorrido (em segundos):', contador);
+    if (contador === 10) {
+        clearInterval(id)
+    }
+}, 1000)
